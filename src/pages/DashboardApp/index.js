78452b57e1615b93f30dbb5af223af5a7ae4fa06
oemanuel <oemanuel@uninorte.edu.co>
2021-10-04 19:49:33 -0500

    grafica del raning y parametros

--- src/pages/DashboardApp/index.js
+++ src/pages/DashboardApp/index.js
@@ -1,45 +1,157 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
-import { makeSelectTopColegios } from './selectors'
-import { obtenerTopColegios } from './actions'
+import {
+  makeSelectTopColegios,
+  makeSelectmejoresColegiosX,
+  makeSelectmejoresColegiosY,
+  makeSelectperiodo,
+  makeSelecttopNumero,
+  makeSelectdepartamento,
+  makeSelectmunicipio,
+  makeSelectmunicipiosData,
+  makeSelectnumeroEstudiantes
+}
+  from './selectors'
+import { obtenerTopColegios, change, obtenerMunicipios } from './actions'
 
 // material
-import { Box, Grid, Container, Typography } from '@material-ui/core';
+import { Box, /*Grid, */Container,/* Typography, */Card, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@material-ui/core';
 // components
 import Page from '../../components/Page';
 import {
-  AppTasks,
-  AppNewUsers,
-  AppBugReports,
-  AppItemOrders,
-  AppNewsUpdate,
-  AppWeeklySales,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppCurrentSubject,
-  AppConversionRates
+  /* AppTasks,
+   AppNewUsers,
+   AppBugReports,
+   AppItemOrders,
+   AppNewsUpdate,
+   AppWeeklySales,
+   AppOrderTimeline,
+   AppCurrentVisits,
+   AppWebsiteVisits,
+   AppTrafficBySite,
+   AppCurrentSubject,
+   AppConversionRates,*/
+  AppMejoresColegios
 } from '../../components/_dashboard/app';
 
 // ----------------------------------------------------------------------
 
-export function DashboardApp({ topColegios, handleObtenerTopColegios }) {
-  const { x, setX } = useState([])
-  const { Y, setY } = useState([])
+export function DashboardApp({ topColegios,
+  handleObtenerTopColegios,
+  mejoresColegiosX,
+  mejoresColegiosY,
+  handleChange,
+  periodo,
+  topNumero,
+  departamento,
+  municipio,
+  handleObtenerMunicipios,
+  municipiosData,
+  numeroEstudiantes
+}) {
   useEffect(() => {
     if (topColegios === undefined) {
       handleObtenerTopColegios()
     }
-  }, [topColegios])
-
+    if (municipiosData === undefined) {
+      handleObtenerMunicipios()
+    }
+  })
   return (
     <Page title="Tu Saber 11°">
       <Container maxWidth="xl">
+        <Card>
+          <Box sx={{ padding: '2rem', paddingBottom: '0' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', paddingBottom: '2rem' }}>
+              <FormControl sx={{ width: 100 }}>
+                <InputLabel id="demo-simple-select-label">Perdiodo</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={periodo}
+                  label="Perdiodo"
+                  name='periodo'
+                  onChange={handleChange}
+                >
+                  <MenuItem value={20202}>20202</MenuItem>
+                  <MenuItem value={20201}>20201</MenuItem>
+                  <MenuItem value={20192}>20192</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ width: 100 }} >
+                <TextField
+                  id="outlined-number"
+                  label="Top número"
+                  name="topNumero"
+                  type="number"
+                  value={topNumero}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl sx={{ width: 300 }} >
+                <InputLabel id="departamentos-select-label">Departamento</InputLabel>
+                <Select
+                  labelId="departamentos-select-label"
+                  id="departamentos-select"
+                  value={departamento}
+                  label="Departamento"
+                  name="departamento"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={''}>Ninguno</MenuItem>
+                  {municipiosData !== undefined && (
+                    municipiosData.map((item, index) => <MenuItem key={index} value={item.departamento}>{item.departamento}</MenuItem>)
+                  )
+                  }
+
+                </Select>
+              </FormControl>
+              <FormControl sx={{ width: 300 }}>
+                <InputLabel id="municipios-select-label">Municipios</InputLabel>
+                <Select
+                  labelId="municipios-select-label"
+                  id="municipios-select"
+                  value={municipio}
+                  label="Municipios"
+                  name="municipios"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={''}>Ninguno</MenuItem>
+                  {
+                    departamento !== '' && (
+                      municipiosData.find(element => element.departamento === departamento)?.ciudades?.map((item, index) => <MenuItem key={index} value={index}>{item}</MenuItem>)
+                    )
+                  }
+
+                </Select>
+              </FormControl>
+              <Button variant="contained" onClick={handleObtenerTopColegios} >Consultar</Button>
+            </Box>
+            <FormControl sx={{ width: 100 }} >
+              <TextField
+                id="outlined-number"
+                label="Estudiantes"
+                name="numeroEstudiantes"
+                type="number"
+                value={numeroEstudiantes}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Box>
+          <AppMejoresColegios x={mejoresColegiosX} y={mejoresColegiosY} />
+        </Card>
+        <>
+          {/*
         <Box sx={{ pb: 5 }}>
           <Typography variant="h4">Hi, Welcome back</Typography>
         </Box>
@@ -88,7 +200,8 @@ export function DashboardApp({ topColegios, handleObtenerTopColegios }) {
           <Grid item xs={12} md={6} lg={8}>
             <AppTasks />
           </Grid>
-        </Grid>
+        </Grid>*/}
+        </>
       </Container>
     </Page>
   );
@@ -97,17 +210,37 @@ export function DashboardApp({ topColegios, handleObtenerTopColegios }) {
 DashboardApp.propTypes = {
   dispatch: PropTypes.func.isRequired,
   topColegios: PropTypes.object,
-  handleObtenerTopColegios: PropTypes.func
+  handleObtenerTopColegios: PropTypes.func,
+  mejoresColegiosX: PropTypes.array,
+  mejoresColegiosY: PropTypes.array,
+  periodo: PropTypes.number,
+  topNumero: PropTypes.string,
+  departamento: PropTypes.string,
+  municipio: PropTypes.string,
+  handleChange: PropTypes.func,
+  municipiosData: PropTypes.array,
+  handleObtenerMunicipios: PropTypes.func,
+  numeroEstudiantes: PropTypes.string
 }
 
 const mapStateToProps = createStructuredSelector({
   topColegios: makeSelectTopColegios(),
+  mejoresColegiosX: makeSelectmejoresColegiosX(),
+  mejoresColegiosY: makeSelectmejoresColegiosY(),
+  periodo: makeSelectperiodo(),
+  topNumero: makeSelecttopNumero(),
+  departamento: makeSelectdepartamento(),
+  municipio: makeSelectmunicipio(),
+  municipiosData: makeSelectmunicipiosData(),
+  numeroEstudiantes: makeSelectnumeroEstudiantes()
 });
 
 function mapDispatchToProps(dispatch) {
   return {
     dispatch,
-    handleObtenerTopColegios: () => dispatch(obtenerTopColegios())
+    handleObtenerTopColegios: () => dispatch(obtenerTopColegios()),
+    handleObtenerMunicipios: () => dispatch(obtenerMunicipios()),
+    handleChange: (event) => dispatch(change(event.target))
   }
 }
 
--- src/pages/DashboardApp/saga.js
+++ src/pages/DashboardApp/saga.js
@@ -1,8 +1,8 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, select } from 'redux-saga/effects';
 import axios from 'axios'
-
-import { OBTENER_TOP_COLEGIOS } from './constants'
-import { obtenerTopColegiosSuccess } from './actions'
+import { makeSelectperiodo, makeSelecttopNumero, makeSelectnumeroEstudiantes } from './selectors'
+import { OBTENER_TOP_COLEGIOS, OBTENER_MUNICIPIOS } from './constants'
+import { obtenerTopColegiosSuccess, obtenerMunicipiosSuccess } from './actions'
 
 const baseUrl = 'http://127.0.0.1:5000'
 const endPoint = '/mejores-colegios'
@@ -15,12 +15,12 @@ function obtenerTopColegios(consulta) {
 }
 function* TopColegiosSaga() {
     const consulta = {
-        periodo: 20202,
+        periodo: yield select(makeSelectperiodo()),
         departamento: null,
         municipio: null,
         puntajes: ["PUNT_GLOBAL"],
-        top: 10,
-        num_estudiantes: 0
+        top: yield select(makeSelecttopNumero()),
+        num_estudiantes: yield select(makeSelectnumeroEstudiantes())
     }
 
     const { response, error } = yield call(obtenerTopColegios, consulta)
@@ -28,5 +28,21 @@ function* TopColegiosSaga() {
     else console.log(error)
 }
 
+function obtenerMunucipios() {
+    return axios
+        .get('https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json')
+        .then(response => ({ response }))
+        .catch(error => ({ error }));
+}
+function* apimunicipiosSaga() {
+    const { response, error } = yield call(obtenerMunucipios)
+    console.log(response.data)
+    if (response) yield put(obtenerMunicipiosSuccess(response.data))
+    else console.log(error)
+}
 
-export const dashboardSagas = [takeLatest(OBTENER_TOP_COLEGIOS, TopColegiosSaga)]
+
+export const dashboardSagas = [
+    takeLatest(OBTENER_TOP_COLEGIOS, TopColegiosSaga),
+    takeLatest(OBTENER_MUNICIPIOS, apimunicipiosSaga)
+]
